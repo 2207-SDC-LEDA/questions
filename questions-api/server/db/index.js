aa78db68171f9002e5dc88b8033c0626655f9e35
hutong <hutong@gmail.com>
2023-02-19 20:17:02 -0600

    add env

--- questions-api/server/db/index.js
+++ questions-api/server/db/index.js
@@ -1,11 +1,15 @@
 const mysql = require('mysql2');
 const Promise = require('bluebird');
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
 const connection = mysql.createConnection({
   host:'18.191.57.215',
   port:'3306',
-  user: 'api',
-  password:'Hr@202301',
+  // user: 'api',
+  // password:'Hr@202301',
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
   // host: '127.0.0.1',
   // user:'root',
   database: 'test'
